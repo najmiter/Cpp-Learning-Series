9acf29317766547b9acc5490fb64becc7e68305f
Najam Ul Hassan <85332859+najmiter@users.noreply.github.com>
2022-08-08 11:21:29 +0500

    Add files via upload

---
+++ 01.Intro_1st_Program.cpp
@@ -0,0 +1,37 @@
+// ---------------------------
+//       WHAT IS COMPILER
+// ---------------------------
+/* 
+
+    A compiler converts code written in a high level language (such as C++) into binary code(0's and 1's) for the computer to understand. 
+
+*/
+
+
+
+
+
+
+
+
+// ---------------------------
+//    PROGRAMMING PARADIGMS
+// ---------------------------
+    // 1. Procedural Programming
+    // 2. Object Oriented Programming
+    // 3. Generic Programming
+    // 4. Game Development
+
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a = 10;
+
+    cout << "The value of a is " << a << endl;
+    
+    return 0;
+}
---
+++ 07.cin_cout.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = 0;
+    float inches = 0.0f;
+    char Grade = 'A';
+
+    cout << "Enter your grade: ";
+    cin >> Grade; // 12
+
+    cout << "Your grade is " << Grade << endl;
+
+    // cout << "\nHello" << endl;
+    
+    
+    return 0;
+}
+
+/* 
+
+The number is {3}
+Total inches are {10.4}
+Your grade is {B}
+
+*/
