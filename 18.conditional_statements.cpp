5e3a432844c6c10d575875e54f1c9424bac997ea
Najam <85332859+najmiter@users.noreply.github.com>
2022-08-11 12:58:37 +0500

    Add files via upload

---
+++ 18.conditional_statements.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool Hungry = false;
+    bool KhanaTastyHai = true;
+
+    int number = 8;
+    cout <<"Enter a number = ";
+    cin >> number;
+    bool result = number % 2;
+
+    if (result) cout << "Odd!\n";
+    else cout << "Even!\n";
+
+    // int age = -12;
+
+    /* if (age == 18){
+        cout << "You can vote!\nYour age is 18\n";
+        // cout << age << endl;
+    }
+    else if (age > 18){
+        cout << "Your can vote!\n";
+    }
+    else if (age<1){
+        cout <<"Bnda bn!\n";
+    }
+    else
+        cout << "Jawan ho k ana!\n"; */
+
+    /* if (Hungry)
+    {
+        cout << "Chlo g!\n";
+        cout << "Pani b peena chahye!\n";
+        cout << "Let's eat\n";
+    }
+    else if (KhanaTastyHai)
+    {
+        cout << "Thora sa chakhaa do!\n";
+    }
+    else
+    {
+        cout << "Mujhe bhook ni h!\n";
+    } */
+
+    // cout << "Life's going great!\n";
+
+    return 0;
+}
