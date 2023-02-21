041497098554094d4217f4d514ee2e1755192451
Najam <85332859+najmiter@users.noreply.github.com>
2023-02-21 12:16:15 +0500

    Update main.cpp

--- 02 OOP/68.subscript_operator/main.cpp
+++ 02 OOP/68.subscript_operator/main.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
-    int64_t array[10];
+    // int64_t array[10];
     Array my_array; // int64_t m_Array[100];
 
-    my_array.push_back(121);
-    my_array.push_back(131);
-    my_array.push_back(141);
-    my_array.push_back(1321);
-    my_array.push_back(13901);
+    my_array.push_back(121LL);
+    my_array.push_back(131LL);
+    my_array.push_back(141LL);
+    my_array.push_back(1321LL);
+    my_array.push_back(13901LL);
 
     for (size_t i = 0; i < my_array.size(); i++)
     {
@@ -20,8 +20,8 @@ int main()
 
     // Array::display_array(my_array);
 
-    // array[0] = 12;
-    // array[1] = 141;
+    // array[0] = 12LL;
+    // array[1] = 141LL;
 
 
 
