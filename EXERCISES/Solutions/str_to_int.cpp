0ef1f96fd71e761834c4fbc842b2090892e53b0f
Najam <85332859+najmiter@users.noreply.github.com>
2023-04-04 10:05:39 +0500

    Update str_to_int.cpp

--- EXERCISES/Solutions/str_to_int.cpp
+++ EXERCISES/Solutions/str_to_int.cpp
@@ -11,18 +11,18 @@ int64_t str_to_int64(std::string_view number) {
     for (auto each : number)
     {
         if (isdigit(each)) break;
-        valid_pos++; // 1
+        valid_pos++;
     }
 
     // Checking for negative number
     if (valid_pos > 0 and number[valid_pos-1] == '-') is_positive = false;
 
     // Actual conversion
-    for (size_t i{valid_pos}; i < number.length() and isdigit(number[i]); i++) // 3, 3
+    for (size_t i{valid_pos}; i < number.length() and isdigit(number[i]); i++)
     {
-        converted_number *= 10;                 // 238
-        converted_number += number[i] - 48;     // 8
-    }
+        converted_number *= 10;
+        converted_number += number[i] - 48;  
+    
 
     return is_positive ? converted_number : -converted_number;
 }
