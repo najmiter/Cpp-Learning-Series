231bd1d6f81ef7626aa4465174750ce1af563fa3
Najam <85332859+najmiter@users.noreply.github.com>
2023-04-04 10:03:51 +0500

    Add files via upload

---
+++ EXERCISES/Solutions/str_to_int.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+#include <string_view>
+
+int64_t str_to_int64(std::string_view number) {
+    int64_t converted_number{};
+    bool is_positive{ true };
+    size_t valid_pos{};
+
+    // Skipping to the actual number (if found)
+    for (auto each : number)
+    {
+        if (isdigit(each)) break;
+        valid_pos++; // 1
+    }
+
+    // Checking for negative number
+    if (valid_pos > 0 and number[valid_pos-1] == '-') is_positive = false;
+
+    // Actual conversion
+    for (size_t i{valid_pos}; i < number.length() and isdigit(number[i]); i++) // 3, 3
+    {
+        converted_number *= 10;                 // 238
+        converted_number += number[i] - 48;     // 8
+    }
+
+    return is_positive ? converted_number : -converted_number;
+}
+
+int main() {
+    std::string str_number{ "      32fw5eoi" };
+
+    auto number {str_to_int64(str_number)};
+
+    std::cout << number << '\n';
+
+}
